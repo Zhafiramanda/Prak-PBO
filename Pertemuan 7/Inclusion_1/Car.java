aafb778100717b84aaf71bb97fdcd4debe478179
Zhafiramanda <100135755+Zhafiramanda@users.noreply.github.com>
2023-05-08 14:44:51 +0700

    Update Car.java

--- Pertemuan 7/Inclusion_1/Car.java
+++ Pertemuan 7/Inclusion_1/Car.java
@@ -1,3 +1,9 @@
+/**
+  * File : Car.java 03/05/2023
+  * Penulis : Zhafira Amanda
+  * Deskripsi : Kelas Car yang merupakan Turunan dari kelas Vehicle
+ **/
+
 public class Car extends Vehicle{
 	void calRent(int jarak, float harga){
 		float fare=jarak*harga;
