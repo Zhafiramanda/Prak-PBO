cdcf9995e246833707c38c74327b2763d6ea5b15
Zhafiramanda <100135755+Zhafiramanda@users.noreply.github.com>
2023-05-08 14:46:28 +0700

    Update Vehicle.java

--- Pertemuan 7/Inclusion_1/Vehicle.java
+++ Pertemuan 7/Inclusion_1/Vehicle.java
@@ -1,3 +1,9 @@
+/**
+  * File : Vehicle.java 03/05/2023
+  * Penulis : Zhafira Amanda
+  * Deskripsi : SuperClass dari kelas Car dan Bus
+ **/
+
 public class Vehicle{
 	void calRent(int distance, float price){
 		float fare=distance*price;
