3c01e1780c9d9e672ed0c2684f669f3818908307
Zhafiramanda <100135755+Zhafiramanda@users.noreply.github.com>
2023-05-08 14:47:31 +0700

    Update PolimorfismeCoercion.java

--- Pertemuan 7/Coercion/PolimorfismeCoercion.java
+++ Pertemuan 7/Coercion/PolimorfismeCoercion.java
@@ -1,3 +1,9 @@
+/**
+  * File : PolimorfismeCoercion.java 03/05/2023
+  * Penulis : Zhafira Amanda
+  * Deskripsi : Kelas PolimorfismeCoercion
+ **/
+
 public class PolimorfismeCoercion{
 	
 	public static int kuadarat(int bilangan){
