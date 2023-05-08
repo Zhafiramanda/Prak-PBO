a67b4ce063359e45e38dc5b60704694cd3e4e13a
Zhafiramanda <100135755+Zhafiramanda@users.noreply.github.com>
2023-05-08 14:37:57 +0700

    Update Payroll.java

--- Pertemuan 7/Inclusion_2/Payroll.java
+++ Pertemuan 7/Inclusion_2/Payroll.java
@@ -1,3 +1,9 @@
+/**
+  * File : Payroll.java 07/05/2023
+  * Penulis : Zhafira Amanda
+  * Deskripsi : Kelas Payroll
+ **/
+
 public class Payroll {
     public void cetakGaji(Pegawai p) {
 		p.tampilData();
