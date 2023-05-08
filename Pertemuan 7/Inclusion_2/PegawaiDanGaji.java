d2aed1bc4743d961aea30a1a3cb2fdb46126eeeb
Zhafiramanda <100135755+Zhafiramanda@users.noreply.github.com>
2023-05-08 14:40:02 +0700

    Update PegawaiDanGaji.java

--- Pertemuan 7/Inclusion_2/PegawaiDanGaji.java
+++ Pertemuan 7/Inclusion_2/PegawaiDanGaji.java
@@ -1,3 +1,9 @@
+/**
+  * File : PegawaiDanGaji.java 07/05/2023
+  * Penulis : Zhafira Amanda
+  * Deskripsi : Kelas untuk MPegawaiDanGaji
+ **/
+
 public class PegawaiDanGaji{
   public static void main(String args[]){
     Pegawai pegawai = new Programmer("Mira");
